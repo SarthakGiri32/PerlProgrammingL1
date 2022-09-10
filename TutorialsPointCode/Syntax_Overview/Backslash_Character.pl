c10ea7ae0513b8a9db9bd4aa540db5a74ab95f15
Sarthak Giri <giri.sarthak1@gmail.com>
2022-09-10 20:45:43 +0530

    Adding tutorial code for backslash character

--- TutorialsPointCode/Syntax_Overview/Backslash_Character.pl
+++ TutorialsPointCode/Syntax_Overview/Backslash_Character.pl
@@ -1,3 +1,8 @@
 #!/usr/bin/perl
 use strict;
 use warnings FATAL => 'all';
+
+my $backslashUseExample = "This is an example of a \"double quotes\" inside double quotes using backslash character";
+print("Example without escaping dollar sign: $backslashUseExample");
+print("\nExample without escaping dollar sign: \$backslashUseExample");
+
