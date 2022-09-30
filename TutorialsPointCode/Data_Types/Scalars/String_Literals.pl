89ddabc7106032c7ffc1d9d49c976d743147b844
Sarthak Giri <giri.sarthak1@gmail.com>
2022-09-30 14:37:00 +0530

    adding final lines of code to the String_Literals.pl file

--- TutorialsPointCode/Data_Types/Scalars/String_Literals.pl
+++ TutorialsPointCode/Data_Types/Scalars/String_Literals.pl
@@ -14,9 +14,6 @@ $str_literal = "Welco\ume t\uo the sc\uript for \n\t\"Str\uing li\uterals\"";
 print("$str_literal\n");
 
 # "\U" forces all following characters to become upper case
-$str_literal = "Welcome to the \Uscript for \n\t\"String literals\"";
-print("$str_literal\n");
-
 $str_literal = "\UWelcome to the script for \n\t\"String literals\"";
 print("$str_literal\n");
 
@@ -24,8 +21,20 @@ print("$str_literal\n");
 $str_literal = "Welcome to \Uthe script for \n\t\"String\E literals\"";
 print("$str_literal\n");
 
-# "\l" character only capitalizes the following character
+# "\l" character only makes the following character small
 $str_literal = "WEL\lCOME \lTO T\lHE SC\lRI\lPT \lFOR \n\t\"\lSTRI\lNG LIT\lER\lAL\lS\"";
 print("$str_literal\n");
 
-#
+# "\L" character makes all the following characters small, unless used with "\E" to limit its scope
+$str_literal = "\LWELCOME TO THE SCRIPT FOR \n\t\"STRING LITERALS\"";
+print("$str_literal\n");
+
+$str_literal = "WELCOME \LTO THE SCRIPT FOR\E \n\t\"STRING LITERALS\"";
+print("$str_literal\n");
+
+# "\Q" is used to add backspace to any non-alphanumeric characters
+$str_literal = "\QString Literal's script welcomes you!";
+print("$str_literal\n");
+
+$str_literal = "String Lite\Qral's script wel\Ecomes you!";
+print("$str_literal\n");
